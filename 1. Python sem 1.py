78745d140a3c893b21e8a3df594e42a5207a6134
jpqb <53953023+jpqb@users.noreply.github.com>
2021-11-01 15:55:29 -0300

    Add files via upload

---
+++ 1. Python sem 1.py
@@ -0,0 +1,7 @@
+import pandas as pd
+# read the online file by the URL provided above, and assign it to variable "df"
+path="https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
+
+df = pd.read_csv(path,header=None)
+
+df.head()
